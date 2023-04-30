66fa25bd634486cf33d16c6a0584b0d48fc48b75
Altiordev <akbarxon2005@gmail.com>
2023-04-30 17:19:44 +0500

    To Do App

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,139 +1,200 @@
 /** @format */
 import { useState } from "react";
-
 import "../Header/header.scss";
 
 const Header = () => {
-  const [changeMode, setChangeMode] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [showMenu, setShowMenu] = useState(false);
-
-  function changeColor() {
-    setChangeMode(!changeMode);
-  }
-  const openMenu = () => setShowMenu(true);
-  const closeMenu = () => setShowMenu(false);
+  const [text, setText] = useState(false);
+  const [id, setId] = useState("");
 
   const openModal = () => {
     setShowModal(true);
-    closeMenu();
+    setText(false);
   };
   const closeModal = () => setShowModal(false);
 
-  const handleOutsideClick = (e) => {
+  function handleCloseModal(e) {
     if (e.target.className === "modal active") {
       closeModal();
     }
+  }
+
+  const [todosFirstName, setTodosFirstName] = useState("");
+  const [todosLastName, setTodosLastName] = useState("");
+  const [todosAge, setTodosAge] = useState("");
+
+  const handleChangeInputF = (e) => {
+    setTodosFirstName(e.target.value);
+  };
+  const handleChangeInputL = (e) => {
+    setTodosLastName(e.target.value);
+  };
+  const handleChangeInputA = (e) => {
+    setTodosAge(e.target.value);
   };
 
-  document.body.classList = changeMode ? "light" : "dark";
+  const [todos, setTodos] = useState(() => {
+    const localItem = localStorage.getItem("todos");
+    return localItem ? JSON.parse(localItem) : [];
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!todosFirstName.trim()) {
+      return alert("Ma'lumot kiritmadingiz!");
+    }
+    if (!todosLastName.trim()) {
+      return alert("Ma'lumot kiritmadingiz!");
+    }
+    if (!todosAge.trim()) {
+      return alert("Ma'lumot kiritmadingiz!");
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        firstName: todosFirstName,
+        lastName: todosLastName,
+        age: todosAge,
+      },
+    ]);
+
+    setTodosFirstName("");
+    setTodosLastName("");
+    setTodosAge("");
+
+    closeModal();
+  };
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  const handleDelete = (id) => {
+    const uptadeLocal = todos.filter((todo) => todo.id !== id);
+
+    setTodos(uptadeLocal);
+  };
+
+  const handleEdit = (id) => {
+    openModal();
+    setText(true);
+    todos.forEach((item) => {
+      if (item.id === id) {
+        setTodosFirstName(item.firstName);
+        setTodosLastName(item.lastName);
+        setTodosAge(item.age);
+      }
+    });
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    if (!todosFirstName.trim()) {
+      return alert("Ma'lumot kiritmadingiz!");
+    }
+    if (!todosLastName.trim()) {
+      return alert("Ma'lumot kiritmadingiz!");
+    }
+    if (!todosAge.trim()) {
+      return alert("Ma'lumot kiritmadingiz!");
+    }
+
+    const updatedTodos = todos.map((item) => {
+      if (item.id === id) {
+        return {
+          id: item.id,
+          firstName: todosFirstName,
+          lastName: todosLastName,
+          age: todosAge,
+        };
+      }
+      return item;
+    });
+
+    setTodos(updatedTodos);
+
+    setTodosFirstName("");
+    setTodosLastName("");
+    setTodosAge("");
+
+    closeModal();
+  };
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  const tr = todos.map((item) => {
+    return (
+      <tr key={item.id}>
+        <td>{item.firstName}</td>
+        <td>{item.lastName}</td>
+        <td>{item.age}</td>
+        <td
+          onClick={() => {
+            handleEdit(item.id);
+            setId(item.id);
+          }}
+        >
+          <i className='fa-solid fa-pencil'></i>
+        </td>
+        <td onClick={() => handleDelete(item.id)}>
+          <i className='fa-solid fa-trash'></i>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <header>
-      <nav>
-        <div className='container'>
-          <div className='nav'>
-            <div className='logo'>
-              {/* <img src={logo} alt='logo' /> */}
-              <svg
-                className={`${changeMode ? "active" : ""}`}
-                width='114'
-                height='24'
-                viewBox='0 0 114 24'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <g clip-path='url(#clip0_1_206)'>
-                  <path
-                    d='M92.25 1.5H108C111.314 1.5 114 4.18629 114 7.5V9C114 12.3137 111.314 15 108 15H96V19.5H114V24H91.5V16.5C91.5 13.1863 94.1863 10.5 97.5 10.5H109.5V6H92.25V1.5Z'
-                    fill='white'
-                  />
-                  <path
-                    d='M7.90358 15.475C10.1925 17.5981 11.625 20.6318 11.625 24H22.5C22.5 16.8762 19.1893 10.5257 14.0227 6.40305L7.90358 15.475Z'
-                    fill='#742BF2'
-                  />
-                  <path
-                    d='M11.2345 4.50116C7.92859 2.59228 4.09185 1.5 0 1.5V12.375C1.8386 12.375 3.57752 12.8018 5.12304 13.5618L11.2345 4.50116Z'
-                    fill='#742BF2'
-                  />
-                  <path
-                    d='M22.5 9C20.0147 9 18 6.98528 18 4.5C18 2.01472 20.0147 0 22.5 0C24.9853 0 27 2.01472 27 4.5C27 6.98528 24.9853 9 22.5 9Z'
-                    fill='#EB864D'
-                  />
-                  <path
-                    d='M58.5 1.5H54V10.5H40.5V1.5H36V9C36 12.3137 38.6863 15 42 15H45V24H49.5V15H52.5C55.8137 15 58.5 12.3137 58.5 9V1.5Z'
-                    fill='white'
-                  />
-                  <path
-                    d='M85.5 1.5H81V10.5H67.5V1.5H63V9C63 12.3137 65.6863 15 69 15H81V24H85.5V15H88.5V10.5H85.5V1.5Z'
-                    fill='white'
-                  />
-                </g>
-                <defs>
-                  <clipPath id='clip0_1_206'>
-                    <rect width='114' height='24' fill='white' />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
-            <div
-              className={`menu ${showMenu ? "active" : ""} ${
-                changeMode ? "activeForBg" : ""
-              }`}
-            >
-              <button
-                className={`closeMenuBtn ${changeMode ? "active" : ""}`}
-                onClick={closeMenu}
-              >
-                <i class='fa-sharp fa-solid fa-caret-up'></i>
+      <div className='container'>
+        <div
+          className={`modal ${showModal ? "active" : ""}`}
+          onClick={handleCloseModal}
+        >
+          <div className={`modal-item ${showModal ? "active" : ""}`}>
+            <i className='fa-solid fa-xmark' onClick={closeModal}></i>
+            <form className='form' onSubmit={handleSubmit}>
+              <input
+                type='text'
+                placeholder='Ism'
+                value={todosFirstName}
+                onChange={handleChangeInputF}
+              />
+              <input
+                type='text'
+                placeholder='Familiya'
+                value={todosLastName}
+                onChange={handleChangeInputL}
+              />
+              <input
+                type='number'
+                placeholder='Yosh'
+                value={todosAge}
+                onChange={handleChangeInputA}
+              />
+              <button type='submit' onClick={text ? handleSave : handleSubmit}>
+                {text ? "O‘zgartirish" : "Qo‘shish"}
               </button>
-              <ul className='menu__ul'>
-                <li className='menu__ul-item'>Products</li>
-                <li className='menu__ul-item'>Resources</li>
-                <li className='menu__ul-item'>Company</li>
-                <li className='menu__ul-item'>
-                  <button
-                    className={`openModal ${changeMode ? "light" : "dark"}`}
-                    onClick={openModal}
-                  >
-                    Modal
-                  </button>
-                </li>
-              </ul>
-            </div>
-            <div className='btns'>
-              <button
-                className={`openModal desktop ${changeMode ? "light" : "dark"}`}
-                onClick={openModal}
-              >
-                Modal
-              </button>
-              <button className='changeMode' onClick={changeColor}>
-                {changeMode ? (
-                  <i class='fa-solid fa-moon'></i>
-                ) : (
-                  <i class='fa-solid fa-sun'></i>
-                )}
-              </button>
-              <button
-                className={`openMenu ${changeMode ? "light" : "dark"}`}
-                onClick={openMenu}
-              >
-                <i class='fa-solid fa-bars'></i>
-              </button>
-            </div>
+            </form>
           </div>
         </div>
-      </nav>
-      <div
-        className={`modal ${showModal ? "active" : ""}`}
-        onClick={handleOutsideClick}
-      >
-        <div className={`modal-item ${showModal ? "active" : ""}`}>
-          <h3>MODAL</h3>
-          <button onClick={closeModal}>
-            <i class='fa-solid fa-xmark'></i>
-          </button>
-        </div>
+        <h1>Foydalanuvchi ma'lumotlari</h1>
+        <button className='openModal addUser' onClick={openModal}>
+          Qo'shish +
+        </button>
+        <table className='fl-table'>
+          <thead>
+            <tr>
+              <th>Ism</th>
+              <th>Familiya</th>
+              <th>Yosh</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody className='tBody'>{tr}</tbody>
+        </table>
       </div>
     </header>
   );
